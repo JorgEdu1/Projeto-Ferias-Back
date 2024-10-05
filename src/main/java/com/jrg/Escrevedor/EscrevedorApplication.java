d9d8f37ab58e7491ae18b3cd04250d796a1e40d9
JorgEdu1 <ej33542@gmail.com>
2024-10-05 18:54:07 -0300

    projeto init

---
+++ src/main/java/com/jrg/Escrevedor/EscrevedorApplication.java
@@ -0,0 +1,13 @@
+package com.jrg.Escrevedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscrevedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscrevedorApplication.class, args);
+	}
+
+}
